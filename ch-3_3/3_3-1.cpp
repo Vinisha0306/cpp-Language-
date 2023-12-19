e00b2d143f288894a0c379db061052f4954a4980
Vinisha0306 <lathiyavinisha03@gmail.com>
2023-12-19 10:47:23 +0530

    19/12/2023

--- ch-3_3/3_3-1.cpp
+++ ch-3_3/3_3-1.cpp
@@ -4,14 +4,42 @@ using namespace std;
 
 class companies
 {
-    int id,staff,year,raw_year,export_year;
+    int Id,staff,year,raw_year,export_year;
     char name[20];
     static char ceo[20];
+    
+    public:
 
-    companies()
+    companies(int Id,char name[],int staff,int year,int raw_year,int export_year)
+    { 
+        this->Id = Id;
+        strcpy(this->name,name);
+        this->staff =staff;
+        this->year = year;
+        this->raw_year = raw_year;
+        this->export_year = export_year;
+
+        cout << "Id" << "\t" << "name" << "\t\t" << "staff"<< "\t" << "year" << "\t" << "raw_year" << "\t" << "export_year" << "\t" << "ceo" << endl;
+        cout << Id << "\t" << name << "\t" << staff << "\t" << year << "\t" <<raw_year << "\t\t" << export_year << "\t\t" << ceo << endl << endl;
+    }
+};
+
+char companies :: ceo[20]="Vinisha Lathiya";
+
+int main()
+{
+    int n;
+    int Id,staff,year,raw_year,export_year;
+    char name[20];
+    
+    cout << "Enter companie number:";
+    cin >> n;
+    cout << endl;
+
+    for(int i=0;i<n;i++)
     {
         cout << "Enter companie ID:";
-        cin >> id;
+        cin >> Id;
         cout << "Enter companie Name:";
         fflush(stdin);
         gets(name);
@@ -23,14 +51,9 @@ class companies
         cin >> raw_year;
         cout << "Enter companie export diamonds(no. of diamonds per year):";
         cin >> export_year;
+        cout << endl;
 
+        companies c1(Id,name,staff,year,raw_year,export_year);
     }
-};
-
-char companies :: ceo[20]="Vinisha Lathiya";
-
-int main()
-{
-    companies c1();
     return 0;
 }
---
+++ pr-2/pr_2-2.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<string.h>
+using namespace std ;
+
+class supermarket
+{
+    int number,quantity,discount,a=0,b=0,c=a,d=b;
+    double price;
+    char name[20],idv[20],pass[10];
+    
+    public:
+    
+    void set_data()
+    {
+        
+        cout << "Enter item number:";
+        cin >> this->number;
+        cout << "Entet item name:";
+        cin >> this->name;
+        cout << "Enter item price:";
+        cin >> this->price;
+        cout << "Enter quantity:";
+        cin >> this->quantity;
+        cout << "Enter discount:";
+        cin >> this->discount;
+        cout << endl;
+        
+    }
+    
+    void verify()
+    {
+        cout <<"Enter your id:";
+        cin >> this->idv;
+        cout << "Enter your password:";
+        cin >> this->pass;
+    }
+    
+    void output(char id[20],char password[10])
+    {
+        
+        verify();
+        
+        for(int i=0;id[i]!=NULL;i++)
+        {
+            if(id[i]==this->idv[i])
+                a++;
+        }
+        for(int i=0;password[i]!=NULL;i++)
+        {
+            if(password[i]==this->pass[i])
+                b++;
+        }
+        
+        if(a==11 && b==6)
+        {
+          
+            cout << "|\tnumber\t|\tname\t|\tprice\t|\tquantity\t|\tdiscount\t| ";
+        } 
+        else
+        {
+            cout << "enter" << endl;
+        }   
+        
+            if(a==11 && b==6)
+        {
+          
+            cout << "|\t" << this->number << "\t|\t" << this->name << "\t|\t"  << this->price << "\t|\t"  << this->quantity << "\t|\t"  << this->discount << "\t| ";
+        } 
+        else
+        {
+            cout << "enter" << endl;
+        }   
+        
+    }
+};
+
+int main()
+{
+    char id[20]="vinisha0306",password[10]="123456";
+    int n;
+    
+    supermarket s1;
+    
+        s1.set_data();
+        s1.output(id,password);
+    
+    
+    return 0;
+}
