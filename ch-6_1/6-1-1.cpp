e9445eff05c70d7d924dc423c79c5006eb002331
Vinisha0306 <lathiyavinisha03@gmail.com>
2023-12-27 12:24:55 +0530

    27/12/2023

---
+++ ch-6_1/6-1-1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+class Admin
+{
+    private:
+        int total_annual_revenue;
+    protected:
+        int manager_salary;
+        int total_staff;
+    public:
+         int employee_salary;
+         char company_name[20];
+};
+
+class Manager : private Admin 
+{
+    void MyAcess()
+    {
+        cin >> manager_salary;
+        cin >> total_staff;
+    }
+    
+    public:
+        int emp_salary;
+        char cmp_name[20];
+
+    Manager()
+    {
+        emp_salary = employee_salary;
+        strcpy(cmp_name,company_name);
+    }
+}; 
+
+class Employee : public Manager
+{
+    void MyAcess()
+    {
+        cin >> emp_salary;
+        cin >> cmp_name;
+    }
+};
